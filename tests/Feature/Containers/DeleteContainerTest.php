f0dd3d35989eb331bd34e8068e1129404e478512
A Gaudot <axel@lespetitsbots.com>
2023-12-28 10:51:59 +0100

    Tests done

---
+++ tests/Feature/Containers/DeleteContainerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Containers;
+
+use App\Models\Container;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\Feature\AbstractTester;
+
+class DeleteContainerTest extends AbstractTester
+{
+    use RefreshDatabase;
+
+    /**
+     * @return void
+     */
+    public function testICanDeleteAContainer(): void
+    {
+        $user = User::factory()->create();
+        $container = Container::factory(['user_id' => $user->id])->create();
+
+        $this->delete("/api/containers/{$container->id}")
+            ->assertStatus(204);
+    }
+
+    /**
+     * @return void
+     */
+    public function testICantDeleteAnUnknownContainer(): void
+    {
+        $this->delete("/api/containers/1")
+            ->assertStatus(404);
+    }
+}
